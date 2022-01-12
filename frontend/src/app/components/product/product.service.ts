0ae13ed4fab9bc963acf9415d2260e0c5a36fdfb
eduardo <ev4lente@gmail.com>
2022-01-12 16:48:43 -0300

    Delete product

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -14,30 +14,33 @@ export class ProductService {
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg: string): void {
-     this.snackBar.open(msg, 'X', {
-       duration: 3000,
-       horizontalPosition: "right",
-       verticalPosition: "top"
-     })
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
   }
 
-  create(product: Product): Observable<Product>
-  {
-     return this.http.post<Product>(this.baseUrl, product)
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product)
   }
 
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
-  readById(id: string): Observable<Product>
-  {
-      const url = `${this.baseUrl}/${id}`
-      return this.http.get<Product>(url)
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
   }
 
   update(product: Product): Observable<Product> {
-    const url = `${this.baseUrl}/${product.id}` 
+    const url = `${this.baseUrl}/${product.id}`
     return this.http.put<Product>(url, product)
+  }
+
+  delete(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
   }
 }
